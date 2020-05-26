c5d5fc8e351d9bb063a5a3619b4c7a595d91e33e
nichrosia <nichrosian@gmail.com>
2020-05-26 17:28:50 -0500

    improved names

--- main.py
+++ main.py
@@ -19,13 +19,11 @@ def create_map(file):
     return junctions, rails
 
 
-def walldots():
+def get_wall_dots(junctions):
     walldots = []
-    junctions, rails = create_map('junctions_rails.txt')
     for junction in junctions:
         walldots.append([[junction[0] - 30, junction[1] - 30], [junction[0] + 30, junction[1] - 30], [junction[0] - 30, junction[1] + 30], [junction[0] + 30, junction[1] + 30]])
     return walldots
-# walldots.append([[topleft], [topright], bottomleft], [bottomright]])
 
 
 def main():
@@ -35,6 +33,7 @@ def main():
     pygame.display.set_caption('PACMAN')
     screen.fill((255, 255, 255))
     black = (0, 0, 0)
+    wall_dots = get_wall_dots(junctions)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -44,8 +43,8 @@ def main():
             pygame.draw.line(screen, (0, 0, 0), junctions[start], junctions[end], 5)
         for junction in junctions:
             pygame.draw.circle(screen, (255, 0, 0), junction, 20, 0)
-        for item in walldots():
-            for walldot in item:
+        for wall_dots_surrounding_junction in wall_dots:
+            for walldot in wall_dots_surrounding_junction:
                 pygame.draw.circle(screen, black, walldot, 5, 0)
 
         pygame.display.flip()
